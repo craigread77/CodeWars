430140e284961488a56827e2d5f21701daeb5520
Craig <craig@localhost.localdomain>
2020-01-28 23:33:31 -0600

    Completed katas

---
+++ Python/increment_string.py
@@ -0,0 +1,33 @@
+import re
+def increment_string(strng):
+    if strng[len(strng)-1].isalpha():
+        return strng + '1'
+    elif strng == '':
+        return '1'
+
+    stringRegex = re.compile(r'([A-Za-z]*)(0*)(\d*)')
+
+    word = stringRegex.match(strng).groups()[0]
+    zeroes = stringRegex.match(strng).groups()[1]
+    num = stringRegex.match(strng).groups()[2]
+
+    if len(num) < 1:
+        zeroes = zeroes[:len(zeroes)-1] + '1'
+
+    else:
+        num_digits = len(num)
+        num = int(num)
+        num += 1
+        num_digits_after = len(str(num))
+
+
+        if num_digits != num_digits_after:
+            if len(zeroes) > 0:
+                zeroes = zeroes [1:]
+
+
+
+
+    return word + zeroes + str(num)
+
+print(increment_string('foo00'))
